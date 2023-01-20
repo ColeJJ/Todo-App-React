11b9cb389f8a166671af2eebae9a732f98f747f3
ColeJJ <torben.unland@gmail.com>
2023-01-20 19:57:21 +0100

    integrated todo component into the home page

--- todo-app/src/app/home/home.module.ts
+++ todo-app/src/app/home/home.module.ts
@@ -1,3 +1,4 @@
+import { TodoComponent } from './../components/todo/todo.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
@@ -6,14 +7,8 @@ import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
 
-
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    HomePageRoutingModule
-  ],
-  declarations: [HomePage]
+	imports: [CommonModule, FormsModule, IonicModule, HomePageRoutingModule],
+	declarations: [HomePage, TodoComponent],
 })
 export class HomePageModule {}
