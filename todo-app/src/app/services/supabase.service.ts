7a5acf3046b717ebaf9b16a9d299b7c3a1e9d412
ColeJJ <torben.unland@gmail.com>
2023-02-23 09:00:45 +0100

    added delete crud functionality to supabase service

--- todo-app/src/app/services/supabase.service.ts
+++ todo-app/src/app/services/supabase.service.ts
@@ -1,30 +1,44 @@
 import { Injectable } from '@angular/core';
-import { createClient, PostgrestResponse, PostgrestSingleResponse, SupabaseClient } from '@supabase/supabase-js';
-import { environment } from "../../environments/environment";
+import {
+	createClient,
+	PostgrestResponse,
+	PostgrestSingleResponse,
+	SupabaseClient,
+} from '@supabase/supabase-js';
+import { environment } from '../../environments/environment';
 
 const TODO_TABLE = 'todos';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root',
 })
 export class SupabaseService {
+	private supabase: SupabaseClient;
 
-  private supabase: SupabaseClient;
+	constructor() {
+		this.supabase = createClient(
+			environment.supabaseURL,
+			environment.supabaseKey
+		);
+	}
 
-  constructor() { 
-    this.supabase = createClient(environment.supabaseURL, environment.supabaseKey);
-  }
+	async getTodos() {
+		const query = await this.supabase.from('todos').select('*');
+		return query.data;
+	}
 
-  async getTodos() {
-    const query = await this.supabase.from('todos').select('*');  
-    return query.data;
-  }
-  
-  async createTodo(description: string): Promise<PostgrestSingleResponse<null>> {
-    const todo = {
-      description: description
-    };
-    const query = await this.supabase.from('todos').insert(todo);
-    return query;
-  } 
+	async createTodo(
+		description: string
+	): Promise<PostgrestSingleResponse<null>> {
+		const todo = {
+			description: description,
+		};
+		const query = await this.supabase.from('todos').insert(todo);
+		return query;
+	}
+
+	async deleteTodo(id: number): Promise<PostgrestSingleResponse<null>> {
+		const query = await this.supabase.from('todos').delete().eq('id', id);
+		return query;
+	}
 }
