75b018c0286d9702316bcdd7b7150e6c9910aed9
ColeJJ <torben.unland@gmail.com>
2023-03-22 13:09:19 +0100

    login: added form validation to email and password input and disabled button until valid

--- todo-app/src/app/pages/login/login.page.ts
+++ todo-app/src/app/pages/login/login.page.ts
@@ -2,6 +2,7 @@ import { SignInWithPasswordCredentials } from '@supabase/supabase-js';
 import { AuthService } from '../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
 	selector: 'app-login',
@@ -11,25 +12,32 @@ import {Router} from "@angular/router";
 export class LoginPage implements OnInit {
 	public email!: string;
 	public password!: string;
+  public loginForm!: FormGroup;
 
-	constructor(private authSevice: AuthService, private router: Router) {}
+	constructor(private authSevice: AuthService, private router: Router, private formBuilder: FormBuilder) {}
 
-	ngOnInit() {}
+	ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      'email': ['',[Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+      'password': ['',[Validators.required, Validators.minLength(6)]]
+    })
+  }
 
 	public async handleLogin() {
-		const credentials: SignInWithPasswordCredentials = {
-			email: this.email,
-			password: this.password,
-		};
-		this.authSevice.signIn(credentials).then((resp) => {
-			if (!resp.error) {
-        this.router.navigateByUrl('/home', {replaceUrl: true})
-        console.log(resp.data.user);
-			} else {
-        console.log(resp.error.message);
-			}
-		});
-		await this.authSevice.signOut();
+    if (this.loginForm.valid) {
+      const credentials: SignInWithPasswordCredentials = {
+        email: this.email,
+        password: this.password,
+      };
+      this.authSevice.signIn(credentials).then((resp) => {
+        if (!resp.error) {
+          this.router.navigateByUrl('/home', {replaceUrl: true})
+          console.log(resp.data.user);
+        } else {
+          console.log(resp.error.message);
+        }
+      });
+    }
 	}
 
   public navToSignUp() {
